3f402c4f441598fbc2b8000bee8e874034b69c30
df.manosalva <df.manosalva@uniandes.edu.co>
2019-02-11 11:21:58 -0500

    Añadido Implementación Resources ObjetivoDTO

--- s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/resources/ObjetivoResource.java
+++ s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/resources/ObjetivoResource.java
@@ -9,8 +9,12 @@ import co.edu.uniandes.csw.requirement.dtos.ObjetivoDTO;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 /**
@@ -29,5 +33,26 @@ public class ObjetivoResource {
     public ObjetivoDTO createObjetivo(ObjetivoDTO objetivo) {
 
         return objetivo;
+    }
+    
+    @GET
+    @Path("{id: \\d+}")
+    public ObjetivoDTO getObjetivo (@PathParam("id") Integer id)
+    {
+        return null;
+    }
+    
+    @DELETE
+    @Path("{id: \\d+}")
+    public void deleteObjetivo (@PathParam("id") Integer id)
+    {
+        
+    }
+    
+    @PUT
+    @Path("{id: \\d+}")
+    public ObjetivoDTO getObjetivo (@PathParam("id") Integer id, ObjetivoDTO dto)
+    {
+        return null;
     }
 }
