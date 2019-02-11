1e5ce4d923ed4e78167dd131b847ed3933c47e62
ms.alvarezl <ms.alvarezl@uniandes.edu.co>
2019-02-11 13:47:56 -0500

    Implementacion de CaminoDTO, CaminoResource, DrsDTO y DrsResource

---
+++ s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/dtos/DrsDTODetail.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.requirement.dtos;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author estudiante
+ */
+public class DrsDTODetail extends DrsDTO implements Serializable
+{
+  //private List<ObjetivoDTO> objetivos;
+  //private List<RequisitoDTO> requisitos;
+  //private List<StakeholderDTO> stakeholders;
+    
+    public DrsDTODetail(){
+        
+    }
+}
---
+++ s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/resources/DrsResource.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.requirement.resources;
+import co.edu.uniandes.csw.requirement.dtos.DrsDTO;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author estudiante
+ */
+@Path("drs")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
+public class DrsResource 
+{
+     private static final Logger LOGGER = Logger.getLogger(DrsResource.class.getName());
+
+     @POST
+    public DrsDTO createDrs(DrsDTO drs) {
+
+        return drs;
+    }
+    
+     @GET
+    @Path("{id: \\d+}")
+    public DrsDTO getDrs (@PathParam("id") Integer id)
+    {
+        return null;
+    }
+    
+     @DELETE
+    @Path("{id: \\d+}")
+    public void deleteDrs (@PathParam("id") Integer id)
+    {
+        
+    }
+    
+    @PUT
+    @Path("{id: \\d+}")
+    public DrsDTO getDrs (@PathParam("id") Integer id, DrsDTO dto)
+    {
+        return null;
+    }
+}
