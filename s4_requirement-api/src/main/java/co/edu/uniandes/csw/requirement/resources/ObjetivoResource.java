04a4a83d3559e2870055c97ec7b0e4239d74ef63
David Manosalva <df.manosalva@uniandes.edu.co>
2019-02-28 16:30:12 -0500

    Modificado Detail Objetivo

--- s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/resources/ObjetivoResource.java
+++ s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/resources/ObjetivoResource.java
@@ -8,21 +8,19 @@ package co.edu.uniandes.csw.requirement.resources;
 
 import co.edu.uniandes.csw.requirement.dtos.ObjetivoDTO;
 import co.edu.uniandes.csw.requirement.dtos.ObjetivoDetailDTO;
+import co.edu.uniandes.csw.requirement.ejb.ObjetivoLogic;
+import co.edu.uniandes.csw.requirement.entities.ObjetivoEntity;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.inject.Inject;
+import javax.ws.rs.*;
 
 /**
  *
- * @author estudiante
+ * @objetivo David Manosalva
  */
 @Path("objetivos")
 @Produces("application/json")
@@ -32,30 +30,78 @@ public class ObjetivoResource {
 
     private static final Logger LOGGER = Logger.getLogger(ObjetivoResource.class.getName());
 
+    @Inject
+    private ObjetivoLogic objetivoLogic;
+
     @POST
     public ObjetivoDTO createObjetivo(ObjetivoDTO objetivo) {
-
-        return objetivo;
+        LOGGER.log(Level.INFO, "ObjetivoResource createObjetivo: input: {0}", objetivo);
+        ObjetivoDTO objetivoDTO = new ObjetivoDTO(objetivoLogic.createObjetivo(objetivo.toEntity()));
+        LOGGER.log(Level.INFO, "ObjetivoResource createObjetivo: output: {0}", objetivoDTO);
+        return objetivoDTO;
     }
 
+    /*
     @GET
-    @Path("{id: \\d+}")
-    public List<ObjetivoDetailDTO> getObjetivo (@PathParam("id") Integer id)
-    {
-        return null;
+    public List<ObjetivoDetailDTO> getObjetivos() {
+        LOGGER.info("ObjetivoResource getObjetivos: input: void");
+        List<ObjetivoDetailDTO> listaObjetivos = listEntity2DTO(objetivoLogic.getObjetivos());
+        LOGGER.log(Level.INFO, "ObjetivoResource getObjetivos: output: {0}", listaObjetivos);
+        return listaObjetivos;
     }
-
-    @DELETE
-    @Path("{id: \\d+}")
-    public void deleteObjetivo (@PathParam("id") Integer id)
-    {
-
+    
+    @GET
+    @Path("{objetivosId: \\d+}")
+    public ObjetivoDetailDTO getObjetivo(@PathParam("objetivosId") Long objetivosId) {
+        LOGGER.log(Level.INFO, "ObjetivoResource getObjetivo: input: {0}", objetivosId);
+        ObjetivoEntity objetivoEntity = objetivoLogic.getObjetivo(objetivosId);
+        if (objetivoEntity == null) {
+            throw new WebApplicationException("El recurso /objetivos/" + objetivosId + " no existe.", 404);
+        }
+        ObjetivoDetailDTO detailDTO = new ObjetivoDetailDTO(objetivoEntity);
+        LOGGER.log(Level.INFO, "ObjetivoResource getObjetivo: output: {0}", detailDTO);
+        return detailDTO;
     }
 
     @PUT
-    @Path("{id: \\d+}")
-    public ObjetivoDTO getObjetivo (@PathParam("id") Integer id, ObjetivoDTO dto)
-    {
-        return null;
+    @Path("{objetivosId: \\d+}")
+    public ObjetivoDetailDTO updateObjetivo(@PathParam("objetivosId") Long objetivosId, ObjetivoDetailDTO objetivo) {
+        LOGGER.log(Level.INFO, "ObjetivoResource updateObjetivo: input: objetivosId: {0} , objetivo: {1}", new Object[]{objetivosId, objetivo});
+        objetivo.setId(objetivosId);
+        if (objetivoLogic.getObjetivo(objetivosId) == null) {
+            throw new WebApplicationException("El recurso /objetivos/" + objetivosId + " no existe.", 404);
+        }
+        ObjetivoDetailDTO detailDTO = new ObjetivoDetailDTO(objetivoLogic.updateObjetivo(objetivosId, objetivo.toEntity()));
+        LOGGER.log(Level.INFO, "ObjetivoResource updateObjetivo: output: {0}", detailDTO);
+        return detailDTO;
     }
+    
+    @DELETE
+    @Path("{objetivosId: \\d+}")
+    public void deleteObjetivo(@PathParam("objetivosId") Long objetivosId) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "ObjetivoResource deleteObjetivo: input: {0}", objetivosId);
+        if (objetivoLogic.getObjetivo(objetivosId) == null) {
+            throw new WebApplicationException("El recurso /objetivos/" + objetivosId + " no existe.", 404);
+        }
+        objetivoLogic.deleteObjetivo(objetivosId);
+        LOGGER.info("ObjetivoResource deleteObjetivo: output: void");
+    }
+    
+    //TODO Uno
+    @Path("{objetivosId: \\d+}/books")
+    public Class<ObjetivoBooksResource> getObjetivoBooksResource(@PathParam("objetivosId") Long objetivosId) {
+        if (objetivoLogic.getObjetivo(objetivosId) == null) {
+            throw new WebApplicationException("El recurso /objetivos/" + objetivosId + " no existe.", 404);
+        }
+        return ObjetivoBooksResource.class;
+    }
+    
+    private List<ObjetivoDetailDTO> listEntity2DTO(List<ObjetivoEntity> entityList) {
+        List<ObjetivoDetailDTO> list = new ArrayList<>();
+        for (ObjetivoEntity entity : entityList) {
+            list.add(new ObjetivoDetailDTO(entity));
+        }
+        return list;
+    }
+    */
 }
