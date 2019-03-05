bf9ae93f819eb517aee52b3561c2fa39f8a3b31d
sofiaalvarezlopez <ms.alvarezl@uniandes.edu.co>
2019-03-04 21:53:16 -0500

    DRSDetailDTO done

--- s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/dtos/DrsDTODetail.java
+++ s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/dtos/DrsDTODetail.java
@@ -5,7 +5,13 @@
  */
 package co.edu.uniandes.csw.requirement.dtos;
 
+import co.edu.uniandes.csw.requirement.entities.DRSEntity;
+import co.edu.uniandes.csw.requirement.entities.ObjetivoEntity;
+import co.edu.uniandes.csw.requirement.entities.RequisitoEntity;
+import co.edu.uniandes.csw.requirement.entities.StakeHolderEntity;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,11 +19,115 @@ import java.io.Serializable;
  */
 public class DrsDTODetail extends DrsDTO implements Serializable
 {
-  //private List<ObjetivoDTO> objetivos;
-  //private List<RequisitoDTO> requisitos;
-  //private List<StakeholderDTO> stakeholders;
+  private List<ObjetivoDTO> objetivos;
+  private List<RequisitoDTO> requisitos;
+  private List<StakeHolderDTO> stakeholders;
     
     public DrsDTODetail(){
         
+    }
+    
+    /**
+     * Constructor para transformar un Entity a un DTO
+     *
+     * @param drsEntity La entidad de la cual se construye el DTO
+     */
+    public DrsDTODetail(DRSEntity drsEntity) {
+        super(drsEntity);
+        if (drsEntity.getObjetivos() != null) {
+            objetivos = new ArrayList<>();
+            for (ObjetivoEntity entityObjetivo : drsEntity.getObjetivos()) {
+                objetivos.add(new ObjetivoDTO(entityObjetivo));
+            }
+        }
+           if (drsEntity.getRequisitos() != null) {
+            requisitos = new ArrayList<>();
+            for (RequisitoEntity entityRequisito : drsEntity.getRequisitos()) {
+                requisitos.add(new RequisitoDTO(entityRequisito));
+            }
+        }
+              if (drsEntity.getStakeholders() != null) {
+            stakeholders = new ArrayList<>();
+            for (StakeHolderEntity entityStakeholder : drsEntity.getStakeholders()) {
+                stakeholders.add(new StakeHolderDTO(entityStakeholder));
+            }
+        }
+
+    }
+    
+    /**
+     * Transformar el DTO a una entidad
+     *
+     * @return La entidad que representa el drs.
+     */
+    @Override
+    public DRSEntity toEntity() {
+        DRSEntity drsEntity = super.toEntity();
+        if (objetivos != null) {
+            List<ObjetivoEntity> objetivosEntity = new ArrayList<>();
+            for (ObjetivoDTO dtoObjetivo : getObjetivos()) {
+                objetivosEntity.add(dtoObjetivo.toEntity());
+            }
+            drsEntity.setObjetivos(objetivosEntity);
+        }
+        if (requisitos != null) {
+            List<RequisitoEntity> requisitosEntity = new ArrayList<>();
+            for (RequisitoDTO dtoRequisito : getRequisitos()) {
+                requisitosEntity.add(dtoRequisito.toEntity());
+            }
+            drsEntity.setRequisitos(requisitosEntity);
+        }
+        if (stakeholders != null) {
+            List<StakeHolderEntity> stakeholdersEntity = new ArrayList<>();
+            for (StakeHolderDTO dtoStakeholder : getStakeholders()) {
+                stakeholdersEntity.add(dtoStakeholder.toEntity());
+            }
+            drsEntity.setStakeholders(stakeholdersEntity);
+        }
+        
+        return drsEntity;
+    }
+    
+
+    /**
+     * @return the objetivos
+     */
+    public List<ObjetivoDTO> getObjetivos() {
+        return objetivos;
+    }
+
+    /**
+     * @param objetivos the objetivos to set
+     */
+    public void setObjetivos(List<ObjetivoDTO> objetivos) {
+        this.objetivos = objetivos;
+    }
+
+    /**
+     * @return the requisitos
+     */
+    public List<RequisitoDTO> getRequisitos() {
+        return requisitos;
+    }
+
+    /**
+     * @param requisitos the requisitos to set
+     */
+    public void setRequisitos(List<RequisitoDTO> requisitos) {
+        this.requisitos = requisitos;
+    }
+
+    /**
+     * @return the stakeholders
+     */
+    public List<StakeHolderDTO> getStakeholders() {
+        return stakeholders;
+    }
+
+    /**
+     * @param stakeholders the stakeholders to set
+     */
+    public void setStakeholders(List<StakeHolderDTO> stakeholders) {
+        this.stakeholders = stakeholders;
     }
 }
