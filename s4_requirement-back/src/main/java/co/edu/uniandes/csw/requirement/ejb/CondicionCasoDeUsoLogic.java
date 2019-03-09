7b37ba9ae2db55cd9b0873db3760b7ab5c70befb
Sofia Sarmiento <s.sarmientob@uniandes.edu.co>
2019-03-09 03:18:04 -0500

    condicion-casoDeUso y camino-casoDeUso relacion resource

--- s4_requirement-back/src/main/java/co/edu/uniandes/csw/requirement/ejb/CondicionCasoDeUsoLogic.java
+++ s4_requirement-back/src/main/java/co/edu/uniandes/csw/requirement/ejb/CondicionCasoDeUsoLogic.java
@@ -5,12 +5,11 @@
  */
 package co.edu.uniandes.csw.requirement.ejb;
 
-import co.edu.uniandes.csw.requirement.entities.CasoDeUsoEntity;
 import co.edu.uniandes.csw.requirement.entities.CondicionEntity;
+import co.edu.uniandes.csw.requirement.entities.CasoDeUsoEntity;
 import co.edu.uniandes.csw.requirement.exceptions.BusinessLogicException;
-import co.edu.uniandes.csw.requirement.persistence.CasoDeUsoPersistence;
 import co.edu.uniandes.csw.requirement.persistence.CondicionPersistence;
-import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import co.edu.uniandes.csw.requirement.persistence.CasoDeUsoPersistence;
 import java.util.logging.Level;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -34,11 +33,9 @@ public class CondicionCasoDeUsoLogic {
      * @return El premio que fue agregado al autor.
      */
     public CasoDeUsoEntity addCasoDeUso(Long casosId, Long condicionesId) {
-        LOGGER.log(Level.INFO, "Inicia proceso de asociar el caso de uso con id = {0} a la condicion con id = " + condicionesId, casosId);
         CasoDeUsoEntity casoEntity = casoPersistence.find(casosId);
         CondicionEntity condicionEntity = condicionPersistence.find(condicionesId);
         condicionEntity.setCasos(casoEntity);
-        LOGGER.log(Level.INFO, "Termina proceso de asociar el caso con id = {0} al camino con id = " + condicionesId, casosId);
         return casoPersistence.find(casosId);
     }
 
@@ -50,9 +47,7 @@ public class CondicionCasoDeUsoLogic {
      * @return el autor solicitada por medio de su id.
      */
     public CasoDeUsoEntity getCasoDeUso(Long condicionesId) {
-        LOGGER.log(Level.INFO, "Inicia proceso de consultar el caso de la condicion con id = {0}", condicionesId);
         CasoDeUsoEntity casoEntity = condicionPersistence.find(condicionesId).getCasos();
-        LOGGER.log(Level.INFO, "Termina proceso de consultar el caso de la condicion con id = {0}", condicionesId);
         return casoEntity;
     }
 
@@ -63,7 +58,6 @@ public class CondicionCasoDeUsoLogic {
      * @throws BusinessLogicException si el premio no tiene autor
      */
     public void removeCasoDeUso(Long condicionesId) throws BusinessLogicException {
-        LOGGER.log(Level.INFO, "Inicia proceso de borrar el caso del camino con id = {0}", condicionesId);
         CondicionEntity condicionEntity = condicionPersistence.find(condicionesId);
         if (condicionEntity.getCasos() == null) {
             throw new BusinessLogicException("La condicion no tiene caso de uso");
@@ -71,7 +65,6 @@ public class CondicionCasoDeUsoLogic {
         CasoDeUsoEntity casoEntity = casoPersistence.find(condicionEntity.getCasos().getId());
         condicionEntity.setCasos(null);
         casoEntity.getCondiciones().remove(condicionEntity);
-        LOGGER.log(Level.INFO, "Termina proceso de borrar el caso con id = {0} del camino con id = " + condicionesId, casoEntity.getId());
     }
     
     
