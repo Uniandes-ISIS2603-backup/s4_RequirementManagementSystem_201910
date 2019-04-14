9164e0e4069247039aa0b0894872d38d00db1e0f
Sofia Sarmiento <s.sarmientob@uniandes.edu.co>
2019-04-13 21:38:52 -0500

    doc api

--- s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/dtos/CondicionDTO.java
+++ s4_requirement-api/src/main/java/co/edu/uniandes/csw/requirement/dtos/CondicionDTO.java
@@ -13,15 +13,34 @@ import java.io.Serializable;
  * @author Sofia Sarmiento
  */
 public class CondicionDTO implements Serializable{
+     /**
+     * Id de la condicion.
+     */
     private Long id;
+    /**
+     * descripcion de la condicion.
+     */
     private String descripcion;
+    /**
+     * seCumplio, variable que indica true si la condicion se cumplio y false de lo contrario.
+     */
     private Boolean seCumplio;
+    /**
+     * esPrecondicion, variable que indica true si la condicion es una precondicion y false de lo contrario.
+     */
     private Boolean esPrecondicion; 
 
+    /**
+     * Constructor vacio
+     */
     public CondicionDTO(){
 
     }
 
+    /**
+     * Crea un Data transfer object (DTO) de condcion a partir de una entity
+     * @param ce entidad con la cual se creará el DTO
+     */
     public CondicionDTO(CondicionEntity ce)
     {
         if(ce != null)
@@ -33,6 +52,10 @@ public class CondicionDTO implements Serializable{
         }
     }
 
+    /**
+     * Convierte un DTO en una entity de 
+     * @return entity construida a partir del DTO
+     */
   public CondicionEntity toEntity() {
         CondicionEntity condicionEntity = new CondicionEntity();
         condicionEntity.setId(this.id);
